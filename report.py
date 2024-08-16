e028301a7589d33064c12788357550ea44f9334d
Min <min@Mins-MacBook-Air.local>
2024-08-16 08:36:41 +0800

    update sheet_name key word wanring

--- report.py
+++ report.py
@@ -8,20 +8,28 @@ from pandas import ExcelWriter
 
 
 class StockScaner:
-    def __init__(self): 
-        #yf.pdr_override()
+    def __init__(self):
+        # yf.pdr_override()
         self.filePath = r"files/report.csv"
-        self.newFile = r'\Users\min\Development\stockscanner\files\output.xlsx'
+        self.newFile = r"output.xlsx"
         self.today = date.today()
         self.start = date.today() + relativedelta(months=-3)
         self.exportList = pd.DataFrame()
         self.solution = pd.DataFrame()
         self.symbols = []
-        self.table = pd.read_csv(self.filePath, names=['Order', 'symbol', 'Name', 'Yoy', 'small cap', 'big cap'], header=None, index_col=None, engine='python')
-        self.table_new = self.table[1:]['symbol'].str.replace('"', '')
-        self.names = self.table[1:]['Name'].str.replace('"', '').to_list()
-        self.data = yf.download(self.dataclean(), start=self.start, end=self.today)['Adj Close']
-        self.daily_returns = self.data.pct_change().sort_values(by=['Date'])
+        self.table = pd.read_csv(
+            self.filePath,
+            names=["Order", "symbol", "Name", "Yoy", "small cap", "big cap"],
+            header=None,
+            index_col=None,
+            engine="python",
+        )
+        self.table_new = self.table[1:]["symbol"].str.replace('"', "")
+        self.names = self.table[1:]["Name"].str.replace('"', "").to_list()
+        self.data = yf.download(self.dataclean(), start=self.start, end=self.today)[
+            "Adj Close"
+        ]
+        self.daily_returns = self.data.pct_change().sort_values(by=["Date"])
 
     def _exportListSheet(self):  # Download Data
         self.exportList = self.exportList._append(self.data)
@@ -31,9 +39,9 @@ class StockScaner:
         symbols = []
         for i in range(len(self.table_new)):
             if int(self.table_new.iloc[i]) > 600000:
-                symbol = self.table_new.iloc[i] + '.SS'
+                symbol = self.table_new.iloc[i] + ".SS"
             else:
-                symbol = self.table_new.iloc[i] + '.SZ'
+                symbol = self.table_new.iloc[i] + ".SZ"
             if len(symbol) == 8:
                 symbol = "0" + symbol
             elif len(symbol) == 7:
@@ -47,25 +55,27 @@ class StockScaner:
             symbols.append(symbol)
         return symbols
 
-    def _solutionSheet(self):     # Solution: Assign attributes
-        self.solution['daily_std'] = self.daily_returns.std()
-        self.solution['daily_average'] = self.daily_returns.mean()
-        self.solution['annual_std'] = self.daily_returns.std() * np.sqrt(252)
-        self.solution['annual_average'] = self.daily_returns.mean() * 252
-        self.solution['ratio'] = self.solution['annual_average'] / self.solution['annual_std']
-        self.solution['names'] = self.names
-        self.solution.sort_values(by=['ratio'], ascending=False, inplace=True)
+    def _solutionSheet(self):  # Solution: Assign attributes
+        self.solution["daily_std"] = self.daily_returns.std()
+        self.solution["daily_average"] = self.daily_returns.mean()
+        self.solution["annual_std"] = self.daily_returns.std() * np.sqrt(252)
+        self.solution["annual_average"] = self.daily_returns.mean() * 252
+        self.solution["ratio"] = (
+            self.solution["annual_average"] / self.solution["annual_std"]
+        )
+        self.solution["names"] = self.names
+        self.solution.sort_values(by=["ratio"], ascending=False, inplace=True)
         self.solution.reset_index(inplace=True)
         return self.solution
 
     def export(self):
         writer = ExcelWriter(self.newFile)
-        self._exportListSheet().to_excel(writer, "Sheet1")
-        self.daily_returns.to_excel(writer, "Daily_returns")
-        self._solutionSheet().to_excel(writer, "Solution")
+        self._exportListSheet().to_excel(writer, sheet_name="Sheet1")
+        self.daily_returns.to_excel(writer, sheet_name="Daily_returns")
+        self._solutionSheet().to_excel(writer, sheet_name="Solution")
         writer._save()
-        os.startfile(self.newFile)
-
+        #os.startfile(self.newFile)
+        print('Report is done!')
 
 Scanner = StockScaner()
 Scanner.export()
--- test.py
+++
@@ -1,5 +0,0 @@
-from yahoo_fin.stock_info import *
-
-df = get_data("TSLA")
-
-print(df)
